038a142f325a67220859157631991dfb1fb6b3bf
Hanneszz <118542657+Hanneszz@users.noreply.github.com>
2023-09-13 16:37:27 +0200

    next steps

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ LINEAR_VELOCITY = 0.1
 DIR_ANGLE_XZ_DEG = -90
 DIR_ANGLE_Y_DEG = -90
 
-ROTATION = 1
+ROTATION = True
 ANGULAR_VELOCITY = np.array([0, 0, -2 * sp.pi])
 
 ## Depth parameters in mm
-START_DEPTH = 0
+START_DEPTH = 100
 END_DEPTH = 125
 STEP_SIZE = 5
-NUM_STEPS = (END_DEPTH - START_DEPTH) / STEP_SIZE
+NUM_STEPS = int((END_DEPTH - START_DEPTH) / STEP_SIZE)
 
 
 ## Miscellaneous & Plots
@@ -69,8 +69,13 @@ SAVE_FIGURES = 0
     object_height,
 ) = rft_functions.import_mesh(MODEL)
 
+result_matrix = np.zeros((NUM_STEPS, 7))
+STEP = 0
+
 ## Start loop
 for depth in range(START_DEPTH, END_DEPTH + STEP_SIZE, STEP_SIZE):
+    point_list[:, 2] -= depth
+    ## Calculate movement
     movement = rft_functions.calc_movement(
         point_list,
         depth,
@@ -82,6 +87,69 @@ for depth in range(START_DEPTH, END_DEPTH + STEP_SIZE, STEP_SIZE):
         ANGULAR_VELOCITY,
     )
 
-    point_list[:, 2] -= STEP_SIZE
+    ## Check conditions
+    (
+        point_list,
+        normal_list,
+        area_list,
+        depth_list,
+        movement,
+    ) = rft_functions.check_conditions(
+        point_list, normal_list, area_list, depth_list, movement
+    )
 
-print("provisional end")
+    ## Find local coordinate frame for each subsurface
+    z_local, r_local, theta_local = rft_functions.find_local_frame(
+        normal_list, movement
+    )
+
+    ## Find the characteristic angles of the RFT method
+    beta, gamma, psi = rft_functions.find_angles(
+        normal_list, movement, z_local, r_local, theta_local
+    )
+
+    ## Find empirical values for the RFT method
+    f_1, f_2, f_3 = rft_functions.find_fit(beta, gamma, psi)
+
+    ## Find dimensionless response vectors alpha
+    alpha_generic, alpha_generic_n, alpha_generic_t, alpha = rft_functions.find_alpha(
+        normal_list,
+        movement,
+        beta,
+        gamma,
+        psi,
+        z_local,
+        r_local,
+        theta_local,
+        f_1,
+        f_2,
+        f_3,
+        MATERIAL_CONSTANT,
+        FRICTION_SURFACE,
+    )
+
+    ## Find the resultant forces on object
+    forces, pressure, force_x, force_y, force_z, resultant = rft_functions.find_forces(
+        alpha, depth_list, area_list
+    )
+
+    ## Find the resultant torques on object
+    (
+        torques,
+        torque_x,
+        torque_y,
+        torque_z,
+        resultant_torque,
+    ) = rft_functions.find_torques(point_list, forces)
+
+    result_matrix[STEP, :] = [
+        depth,
+        force_x,
+        force_y,
+        force_z,
+        torque_x,
+        torque_y,
+        torque_z,
+    ]
+
+    STEP += 1
--- src/rft_functions.py
+++ src/rft_functions.py
@@ -7,34 +7,45 @@ import numpy as np
 from stl import mesh
 
 
-## STL file import and geometric evaluation
-def import_mesh(model):
-    """Caluclating geometric properties of the 3D Object and initial alignment"""
-    trg = mesh.Mesh.from_file(f"./models/{model}.stl")
-    mins = np.amin(trg.centroids, axis=0)
-    maxs = np.amax(trg.centroids, axis=0)
-    object_dims = maxs - mins
-    object_width_x, object_width_y, object_height = object_dims
+def import_mesh(model: str):
+    """Calculates the geometric properties of a 3D object from an STL file.
 
-    point_list = trg.centroids
-    normal_list = trg.normals
-    area_list = trg.areas
+    Args:
+        model (str): The path to the STL file.
 
-    point_list[:, 2] -= min(point_list[:, 2])
-    depth_list = point_list[:, 2]
+    Returns:
+        tuple: A tuple of the geometric properties of the object,
+        including the point cloud, normals, areas, depths, width, height, and length.
+    """
+    try:
+        trg = mesh.Mesh.from_file(f"./models/{model}.stl")
+        mins = np.amin(trg.centroids, axis=0)
+        maxs = np.amax(trg.centroids, axis=0)
+        object_dims = maxs - mins
+        object_width_x, object_width_y, object_height = object_dims
 
-    return (
-        point_list,
-        normal_list,
-        area_list,
-        depth_list,
-        object_width_x,
-        object_width_y,
-        object_height,
-    )
+        point_list = trg.centroids
+        normal_list = trg.normals
+        area_list = trg.areas
+
+        point_list[:, 2] -= min(point_list[:, 2])
+        depth_list = point_list[:, 2]
+
+        return (
+            point_list,
+            normal_list,
+            area_list,
+            depth_list,
+            object_width_x,
+            object_width_y,
+            object_height,
+        )
+    except FileNotFoundError as exc:
+        print(exc)
+        print("Please make sure the model is in the models folder and try again.")
+        exit()
 
 
-## Movement vector determination
 def calc_movement(
     points,
     depth,
@@ -45,7 +56,25 @@ def calc_movement(
     rotation,
     angular_velocity,
 ):
-    """Calculates Movement based on various input parameters"""
+    """Calculates the movement of a set of points based on various input parameters.
+
+    Args:
+        points (np.ndarray): The set of points to move.
+        depth (float): The depth of the object.
+        object_height (float): The height of the object.
+        dir_angle_xz_deg (float): The direction angle in the XZ plane in degrees.
+        dir_angle_y_deg (float): The direction angle in the Y direction in degrees.
+        linear_velocity (float): The linear velocity.
+        rotation (bool): Whether or not the object is rotating.
+        angular_velocity (float): The angular velocity.
+
+    Returns:
+        np.ndarray: The set of points after movement.
+    """
+
+    if not isinstance(rotation, bool):
+        raise TypeError("rotation must be a boolean value.")
+
     linear_direction_vector = np.array(
         [
             [
@@ -69,3 +98,299 @@ def calc_movement(
     movement = (movement / np.linalg.norm(movement, axis=1, keepdims=True)).round(12)
 
     return movement
+
+
+def check_conditions(point_list, normal_list, area_list, depth_list, movement):
+    """This checks certain conditions for subsurfaces to be considered in the RFT method"""
+    is_leading_edge = np.sum(normal_list * movement, axis=1) >= 0
+    is_intruding = point_list[:, 2] < 0
+    include = is_intruding & is_leading_edge
+
+    point_list = point_list[include, :]
+    normal_list = normal_list[include, :]
+    area_list = area_list[include, :]
+    depth_list = depth_list[include]
+    movement = movement[include, :]
+
+    print("rtest")
+
+    return point_list, normal_list, area_list, depth_list, movement
+
+
+def find_local_frame(normal_list, movement):
+    """This determines the local coordinate frame for each subsurface of the object"""
+    z_local = np.repeat([[0, 0, 1]], normal_list.shape[0], axis=0)
+
+    r_local = np.zeros_like(z_local)
+
+    dot_product_movement = np.einsum("ij,ij->ij", movement, z_local)
+    difference_movement = movement - dot_product_movement * z_local
+    norms_movement = np.linalg.norm(difference_movement, axis=1, keepdims=True)
+
+    dot_product_normal = np.einsum("ij,ij->ij", normal_list, z_local)
+    difference_normal = normal_list - dot_product_normal * z_local
+    norms_normal = np.linalg.norm(difference_normal, axis=1, keepdims=True)
+
+    temp_mask_1 = norms_movement == 0
+    temp_mask_2 = norms_normal == 0
+
+    mask_1 = np.squeeze(temp_mask_1 & temp_mask_2)
+    mask_2 = np.squeeze(temp_mask_1 & ~temp_mask_2)
+    mask_3 = np.squeeze(~mask_1 & ~mask_2)
+
+    r_local[mask_1] = [1, 0, 0]
+    r_local[mask_2] = difference_normal[mask_2] / norms_normal[mask_2]
+    r_local[mask_3] = difference_movement[mask_3] / norms_movement[mask_3]
+
+    theta_local = np.cross(z_local, r_local, axis=1)
+
+    return z_local, r_local, theta_local
+
+
+def find_angles(
+    normal_list: np.array,
+    movement: np.array,
+    z_local: np.array,
+    r_local: np.array,
+    theta_local: np.array,
+) -> (np.array, np.array, np.array):
+    """This determines the characteristic angles of the RFT method"""
+    beta = np.zeros(normal_list.shape[0])
+    gamma = np.zeros(normal_list.shape[0])
+    psi = np.zeros(normal_list.shape[0])
+
+    dot_normals_r = np.sum(normal_list * r_local, axis=1, keepdims=True)
+    dot_normals_z = np.sum(normal_list * z_local, axis=1, keepdims=True)
+
+    mask_1 = np.squeeze((dot_normals_r >= 0) & (dot_normals_z >= 0))
+    mask_2 = np.squeeze((dot_normals_r >= 0) & (dot_normals_z < 0))
+    mask_3 = np.squeeze((dot_normals_r < 0) & (dot_normals_z >= 0))
+
+    beta[mask_1] = -np.round(np.arccos(dot_normals_z[mask_1]), 12)
+    beta[mask_2] = np.round(np.pi - np.arccos(dot_normals_z[mask_2]), 12)
+    beta[mask_3] = np.round(np.arccos(dot_normals_z[mask_3]), 12)
+    beta[~(mask_1 | mask_2 | mask_3)] = np.round(
+        -np.pi + np.arccos(dot_normals_z[~(mask_1 | mask_2 | mask_3)]), 12
+    )
+
+    dot_movement_r = np.sum(movement * r_local, axis=1, keepdims=True)
+    dot_movement_z = np.sum(movement * z_local, axis=1, keepdims=True)
+
+    gamma[:] = np.round(np.arccos(np.clip(dot_movement_r, -1, 1)), 12)
+    gamma[dot_movement_z > 0] *= -1
+
+    diff_normals = normal_list - dot_normals_z * z_local
+    norm_diff_normals = np.linalg.norm(diff_normals, axis=1, keepdims=True)
+
+    nr0_inc = np.where(
+        norm_diff_normals != 0,
+        diff_normals / norm_diff_normals,
+        0,
+    )
+    dot_nr0_r = np.sum(nr0_inc * r_local, axis=1, keepdims=True)
+    dot_nr0_theta = np.sum(nr0_inc * theta_local, axis=1, keepdims=True)
+
+    mask_psi = norm_diff_normals == 0 | (dot_nr0_r == 0)
+    psi[~mask_psi] = np.round(
+        np.arctan(dot_nr0_theta[~mask_psi] / dot_nr0_r[~mask_psi]), 12
+    )
+
+    return beta, gamma, psi
+
+
+def find_fit(beta, gamma, psi):
+    """This determines the fit of the RFT method"""
+    x_1 = np.round(np.sin(gamma), 12)
+    x_2 = np.round(np.cos(beta), 12)
+    x_3 = np.round(
+        np.cos(psi) * np.cos(gamma) * np.sin(beta) + np.sin(gamma) * np.cos(beta), 12
+    )
+
+    t_k = np.column_stack(
+        (
+            np.ones(beta.shape[0]),
+            x_1,
+            x_2,
+            x_3,
+            x_1**2,
+            x_2**2,
+            x_3**2,
+            x_1 * x_2,
+            x_2 * x_3,
+            x_3 * x_1,
+            x_1**3,
+            x_2**3,
+            x_3**3,
+            x_1 * x_2**2,
+            x_2 * x_1**2,
+            x_2 * x_3**2,
+            x_3 * x_2**2,
+            x_3 * x_1**2,
+            x_1 * x_3**2,
+            x_1 * x_2 * x_3,
+        )
+    )
+
+    c1k = np.array(
+        [
+            0.00212,
+            -0.02320,
+            -0.20890,
+            -0.43083,
+            -0.00259,
+            0.48872,
+            -0.00415,
+            0.07204,
+            -0.02750,
+            -0.08772,
+            0.01992,
+            -0.45961,
+            0.40799,
+            -0.10107,
+            -0.06576,
+            0.05664,
+            -0.09269,
+            0.01892,
+            0.01033,
+            0.15120,
+        ]
+    )
+
+    c2k = np.array(
+        [
+            -0.06796,
+            -0.10941,
+            0.04725,
+            -0.06914,
+            -0.05835,
+            -0.65880,
+            -0.11985,
+            -0.25739,
+            -0.26834,
+            0.02692,
+            -0.00736,
+            0.63758,
+            0.08997,
+            0.21069,
+            0.04748,
+            0.20406,
+            0.18589,
+            0.04934,
+            0.13527,
+            -0.33207,
+        ]
+    )
+
+    c3k = np.array(
+        [
+            -0.02634,
+            -0.03436,
+            0.45256,
+            0.00835,
+            0.02553,
+            -1.31290,
+            -0.05532,
+            0.06790,
+            -0.16404,
+            0.02287,
+            0.02927,
+            0.95406,
+            -0.00131,
+            -0.11028,
+            0.01487,
+            -0.02730,
+            0.10911,
+            -0.04097,
+            0.07881,
+            -0.27519,
+        ]
+    )
+
+    f_1 = t_k @ c1k
+    f_2 = t_k @ c2k
+    f_3 = t_k @ c3k
+
+    return f_1, f_2, f_3
+
+
+def find_alpha(
+    normal_list,
+    movement,
+    beta,
+    gamma,
+    psi,
+    z_local,
+    r_local,
+    theta_local,
+    f_1,
+    f_2,
+    f_3,
+    material_constant,
+    friction_surface,
+):
+    """This determines the dimensionless response vectors alpha"""
+    alpha_r_gen = np.round(f_1 * np.sin(beta) * np.cos(psi) + f_2 * np.cos(gamma), 12)
+    alpha_theta_gen = np.round(f_1 * np.sin(beta) * np.sin(psi), 12)
+    alpha_z_gen = np.round(-f_1 * np.cos(beta) - f_2 * np.sin(gamma) - f_3, 12)
+
+    alpha_generic = np.column_stack(
+        (alpha_r_gen * r_local, alpha_theta_gen * theta_local, alpha_z_gen * z_local)
+    )
+
+    dot_product_alpha_normals = np.einsum("ij,ij->i", alpha_generic, -normal_list)
+    mask_1 = dot_product_alpha_normals < 0
+    mask_2 = ~mask_1
+
+    alpha_generic_n = np.zeros(normal_list.shape)
+    alpha_generic_t = np.zeros(normal_list.shape)
+
+    alpha_generic_n[mask_1] = (-dot_product_alpha_normals[mask_1]) * (
+        -normal_list[mask_1]
+    )
+    alpha_generic_t[mask_1] = alpha_generic[mask_1] + alpha_generic_n[mask_1]
+
+    alpha_generic_n[mask_2] = (dot_product_alpha_normals[mask_2]) * (
+        -normal_list[mask_2]
+    )
+    alpha_generic_t[mask_2] = alpha_generic[mask_2] - alpha_generic_n[mask_2]
+
+    dot_product_alpha_t_move = np.einsum("ij,ij->i", alpha_generic_t, -movement)
+    mask_3 = dot_product_alpha_t_move < 0
+    alpha_generic_t[mask_3] *= -1
+
+    alpha = material_constant * (
+        alpha_generic_n
+        + np.min(
+            friction_surface
+            * np.linalg.norm(alpha_generic_n, axis=1, keepdims=True)
+            / np.linalg.norm(alpha_generic_t, axis=1, keepdims=True),
+            1,
+        )
+        * alpha_generic_t
+    )
+
+    return alpha_generic, alpha_generic_n, alpha_generic_t, alpha
+
+
+def find_forces(alpha, depth_list, area_list):
+    """This determines the forces on the object"""
+    forces = alpha * abs(depth_list) * area_list
+    pressure = forces / area_list / 1000000
+
+    force_x = np.sum(forces[:, 0])
+    force_y = np.sum(forces[:, 1])
+    force_z = np.sum(forces[:, 2])
+    resultant = np.linalg.norm([force_x, force_y, force_z])
+
+    return forces, pressure, force_x, force_y, force_z, resultant
+
+
+def find_torques(point_list, forces):
+    """This determines the torques on the object"""
+    torques = np.cross(point_list, forces)
+    torque_x = np.sum(torques[:, 0])
+    torque_y = np.sum(torques[:, 1])
+    torque_z = np.sum(torques[:, 2])
+    resultant_torque = np.linalg.norm([torque_x, torque_y, torque_z])
+
+    return torques, torque_x, torque_y, torque_z, resultant_torque
